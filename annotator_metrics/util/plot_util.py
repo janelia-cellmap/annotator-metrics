2fe80719c09a93aeaef970b3daf99853d330887f
David Ackerman <ackermand@janelia.hhmi.org>
2022-07-05 12:36:53 -0400

    include box plot function

--- annotator_metrics/util/plot_util.py
+++ annotator_metrics/util/plot_util.py
@@ -6,6 +6,9 @@ from matplotlib import pyplot as plt
 import seaborn
 import warnings
 
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 def plot_all_to_all(
     group: str,
@@ -122,3 +125,125 @@ def plot_all_to_all(
             pass
         plt.savefig(f"{output_directory}/{organelle_name}.png", bbox_inches="tight")
         plt.close()
+
+
+def plot_boxplots(all_to_all_info):
+    import pandas as pd
+    df = pd.DataFrame(columns = ['Score Type', 'Organelle', 'Annotator', 'Pair', 'Score'])
+    dict_for_dataframe = {}
+    i = 0
+
+    for key, info in all_to_all_info.items():
+        organelle = key[1]
+        score_type = key[2]
+        scores_matrix = info["scores_matrix"]
+        segmentation_types = info["segmentation_types"]
+        for idx_one,segmentation_type_one in enumerate(segmentation_types):
+            for idx_two,segmentation_type_two in enumerate(segmentation_types):
+                if idx_one != idx_two:
+                    score = scores_matrix[idx_one][idx_two]
+                    dict_for_dataframe[i] = {"Score Type": score_type, "Organelle": organelle, "Annotator": segmentation_type_one, "Pair": f"{segmentation_type_one}_{segmentation_type_two}", "Score": score}
+                    dict_for_dataframe[i+1] = {"Score Type": score_type, "Organelle": organelle, "Annotator": segmentation_type_two, "Pair": f"{segmentation_type_one}_{segmentation_type_two}", "Score": score}
+                    i += 2
+
+    df = pd.DataFrame.from_dict(dict_for_dataframe, "index")
+
+
+    import plotly.express as px
+    import plotly.graph_objects as go
+    import numpy as np
+
+    # print(px.strip(df.loc[df.total_bill>0], x="time", y="total_bill", animation_group="total_bill",color_discrete_sequence = ["red"]).data)
+    fig = go.Figure()
+    annotators = df["Annotator"].unique()
+    for annotator in annotators:
+        fig = fig.add_traces(
+            px.strip(
+                df.loc[df["Annotator"] == annotator],
+                x="Organelle",
+                y="Score",
+                hover_data=["Annotator", "Pair", "Organelle", "Score"],
+                color_discrete_sequence=["red"],
+                stripmode="overlay",
+            ).data
+        )  # color_discrete_sequence = ["blue"], stripmode='overlay')
+    fig.update_traces(
+        {
+            "marker.color": "rgba(0, 0, 0,0)",
+            "marker.size": 3,
+            "marker.line": dict(color="rgba(241, 145, 155,1.0)", width=1),
+        }
+    )
+    for annotator in annotators:
+        fig = fig.add_traces(
+            px.strip(
+                df.loc[df["Annotator"] == annotator],
+                x="Organelle",
+                y="Score",
+                hover_data=["Annotator", "Pair", "Organelle", "Score"],
+                stripmode="overlay",
+            ).data
+        )  # color_discrete_sequence = ["blue"], stripmode='overlay')
+    for idx, trace in enumerate(fig.select_traces()):
+        if idx >= len(annotators):
+            trace.visible = False
+            trace.marker.update(
+                {
+                    "color": "rgba(0, 0, 0,1.0)",
+                    "size": 3,
+                    "line": dict(color="rgba(0, 0, 0,1.0)", width=1),
+                }
+            )
+    fig.add_trace(
+        go.Box(x=df["Organelle"], y=df["Score"], boxpoints=False, marker_color="red")
+    )
+    # https://stackoverflow.com/questions/68894919/how-to-set-the-values-of-args-and-args2-in-plotlys-buttons-in-updatemenus
+    updatemenus = []
+    button_layer_y = -0.2
+    for idx, annotator in enumerate(annotators):
+        if idx % 5 == 0:
+            button_layer_x = 1
+            button_layer_y -= 0.3
+
+        button_layer_x -= 0.2
+        updatemenus.append(
+            {
+                "buttons": [
+                    {
+                        "method": "update",
+                        "label": f"Toggle {annotator}",
+                        "args": [
+                            # 1. updates to the traces
+                            {"visible": False},
+                            # 2. updates to the layout
+                            {},
+                            # 3. which traces are affected
+                            [idx + len(annotators)],
+                        ],
+                        "args2": [
+                            {"visible": True},
+                            # 2. updates to the layout
+                            {},
+                            # 3. which traces are affected
+                            [idx + len(annotators)],
+                        ],
+                    }
+                ],
+                "type": "buttons",
+                "direction": "down",
+                "showactive": True,
+                "xanchor": "left",
+                "x": button_layer_x,
+                "y": button_layer_y,
+                "yanchor": "top",
+            }
+        )
+
+    fig.update_layout(
+        updatemenus=updatemenus,
+        title="tmp",
+        xaxis_title="Organelle",
+        yaxis_title=score_type,
+        showlegend=False,
+    )
+    fig.write_html("mine.html")
